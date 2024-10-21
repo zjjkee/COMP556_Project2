d015fda1b616880ae19c83d3e0fcbfa6f4bbc1d2
zjjkee <92360567+zjjkee@users.noreply.github.com>
2024-10-21 10:42:11 -0500

    Update sendfile.c

--- sendfile.c
+++ sendfile.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <getopt.h>
+#include <sys/time.h>
+#include <time.h>
 
 #define PACKET_SIZE 1476
 #define HEADER_SIZE 16  // 4 bytes for sequence number + 4 bytes for checksum + 4 bytes for data length + 4 bytes for  is_last_packet
@@ -18,6 +20,13 @@ struct Packet {
     uint32_t checksum;
     int is_last_packet;  // Flag indicating if this is the last packet
 };
+
+//record current time
+double get_current_time() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
 
 // Function to calculate CRC32 checksum
 uint32_t crc32(const char *data, size_t length) {
@@ -81,19 +90,22 @@ void send_file(int sock, struct sockaddr_in *receiver_addr, const char *file_pat
         return;
     }
 
+    //simulationg for drop
+    float drop_rate = 0.8;  //  丢包率 Drop rate
+    srand(time(NULL));  // 初始化随机数生成器
+
     struct Packet packets[WINDOW_SIZE];
     uint32_t base = 0, next_seq_num = 0;
     // int acked[WINDOW_SIZE] = {0};  // Marks ACK'd packets
     socklen_t addr_len = sizeof(*receiver_addr);
     fd_set fds;
-    struct timeval tv;
-    tv.tv_sec = TIMEOUT;
-    tv.tv_usec = 0;
+    double send_times[WINDOW_SIZE];
 
     while (1) {
         // Continue sending packets as long as there is space in the window
         while (next_seq_num < base + WINDOW_SIZE) {
             size_t read_bytes = fread(packets[next_seq_num % WINDOW_SIZE].data, 1, PACKET_SIZE, file);
+            if (read_bytes <= 0) break;  // File reading complete
             // printf("read_bytes: %zu\n", read_bytes);
 
             // Check if we have reached the end of the file
@@ -104,41 +116,70 @@ void send_file(int sock, struct sockaddr_in *receiver_addr, const char *file_pat
                 packets[next_seq_num % WINDOW_SIZE].is_last_packet = 0;  // 0 indicates a regular packet
             }
 
-            if (read_bytes <= 0) break;  // File reading complete
-            printf("read_bytes: %zu\n", read_bytes);
             packets[next_seq_num % WINDOW_SIZE].sequence_number = next_seq_num;
             packets[next_seq_num % WINDOW_SIZE].data_length = read_bytes;
             packets[next_seq_num % WINDOW_SIZE].checksum = crc32(packets[next_seq_num % WINDOW_SIZE].data, read_bytes);
 
+            // simulation for drop packets
+            float rand_val = (float)rand() / RAND_MAX;  // random number range [0,1]
+            if (rand_val < drop_rate) {
+                printf("[drop packet] %u\n", next_seq_num * PACKET_SIZE);  // log drop
+            } else {
+                // Send the packet to the receiver
+                sendto(sock, &packets[next_seq_num % WINDOW_SIZE], sizeof(struct Packet), 0, (struct sockaddr *)receiver_addr, addr_len);
+                print_send_message(next_seq_num * PACKET_SIZE, read_bytes);
+            }
+
             // Send the packet to the receiver
             sendto(sock, &packets[next_seq_num % WINDOW_SIZE], sizeof(struct Packet), 0, (struct sockaddr *)receiver_addr, addr_len);
             print_send_message(next_seq_num * PACKET_SIZE, read_bytes);
+            send_times[next_seq_num % WINDOW_SIZE] = get_current_time(); // 记录发送时间 // record sending time
             next_seq_num++;
             printf("next_seq_num: %d; base: %d; WINDOW_SIZE: %d \n", next_seq_num, base, WINDOW_SIZE);
         }
         // printf("Waiting for ACK...\n");
 
-        // Wait for ACK
-        FD_ZERO(&fds);
-        FD_SET(sock, &fds);
-        int activity = select(sock + 1, &fds, NULL, NULL, &tv);
-
-        if (activity > 0) {
-            uint32_t ack;
-            int bytes_rcvd = recvfrom(sock, &ack, sizeof(ack), 0, (struct sockaddr *)receiver_addr, &addr_len);
-            if (bytes_rcvd > 0 && ack >= base) {
-                // Slide the window
-                base = ack + 1;
-                // printf("Sliding window, base: %d\n", base);
+        while (1)
+        {
+            // Wait for ACK
+            struct timeval timeout;
+            timeout.tv_sec = TIMEOUT;
+            timeout.tv_usec = 0;
+            FD_ZERO(&fds);
+            FD_SET(sock, &fds);
+            //using select wait ACK or timeout
+            int activity = select(sock + 1, &fds, NULL, NULL, &timeout);
+        // printf(" activity: %d \n", activity);
+            if (activity > 0) {
+                uint32_t ack;
+                int bytes_rcvd = recvfrom(sock, &ack, sizeof(ack), 0, (struct sockaddr *)receiver_addr, &addr_len);
+                // printf("ack:%d, base:%d",ack,base );
+                if (bytes_rcvd > 0 && ack == base) {
+                    // Slide the window
+                    base = ack + 1;
+                    // printf("Sliding window, base: %d\n", base);
+                    // printf(" ACK RECEIVED!!!!!\n");
+                }
+                // when packets are all acked, break
+                if (base == next_seq_num) {
+                    break;
+                }
             }
-        } else {
-            // Retransmit on timeout
-            for (uint32_t i = base; i < next_seq_num; i++) {
-                printf("Retransmitting on timeout\n");
-                sendto(sock, &packets[i % WINDOW_SIZE], sizeof(struct Packet), 0, (struct sockaddr *)receiver_addr, addr_len);
-                print_send_message(i * PACKET_SIZE, PACKET_SIZE);
+            else {
+                // Retransmit on timeout
+                double current_time = get_current_time();
+                for (uint32_t i = base; i < next_seq_num; i++) {
+                        if (current_time - send_times[i % WINDOW_SIZE] >= TIMEOUT) {
+                        printf("Retransmitting on timeout\n");
+                        sendto(sock, &packets[i % WINDOW_SIZE], sizeof(struct Packet), 0, (struct sockaddr *)receiver_addr, addr_len);
+                        print_send_message(i * PACKET_SIZE, PACKET_SIZE);
+                        }
+                }
+                break;  // continue wait ACK after retransmitting
             }
         }
+        
+
 
         // Exit after sending the file completely
         if (feof(file) && base >= next_seq_num) {
